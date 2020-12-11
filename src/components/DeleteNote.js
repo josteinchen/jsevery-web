7fa29bb163f832d8d7cc89dbb69a99013d4cfa23
jostein <jostein.chen@gmail.com>
2020-12-11 16:05:30 +0800

    completed jseverywhere-web project

---
+++ src/components/DeleteNote.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useMutation } from '@apollo/client';
+import { withRouter } from 'react-router-dom';
+import { DELETE_NOTE } from '../gql/mutation';
+import { GET_MY_NOTES, GET_NOTES } from '../gql/query';
+import ButtonAsLink from './ButtonAsLink';
+
+const DeleteNote = props => {
+  const [deleteNote] = useMutation(DELETE_NOTE, {
+    variables: {
+      id: props.noteId
+    },
+    refetchQueries: [{ query: GET_MY_NOTES }, { query: GET_NOTES }],
+    onCompleted: data => {
+      props.history.push('/mynotes');
+    }
+  });
+  return <ButtonAsLink onClick={deleteNote}>Delete Note </ButtonAsLink>;
+};
+
+export default withRouter(DeleteNote);
---
+++ src/pages/edit.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import NoteForm from '../components/NoteForm';
+import { GET_NOTE, GET_ME } from '../gql/query';
+import { EDIT_NOTE } from '../gql/mutation';
+
+const EditNote = props => {
+  const id = props.match.params.id;
+  // define our note query
+  const { loading, error, data } = useQuery(GET_NOTE, { variables: { id } });
+  // fetch the current user's data
+  const { data: userdata } = useQuery(GET_ME);
+  const [editNote] = useMutation(EDIT_NOTE, {
+    variables: {
+      id
+    },
+    onCompleted: () => {
+      props.history.push(`/note/${id}`);
+    }
+  });
+  // if the data is loading, display a loading message
+  if (loading) return 'Loading...';
+  // if there is an error fetching the data, display an error message
+  if (error) return <p>Error! Note not found</p>;
+  // if the current user and the author of the note do not match
+  if (userdata.me.id !== data.note.author.id) {
+    return <p>You do not have access to edit this note</p>;
+  }
+  // pass the data to the form component
+  return <NoteForm content={data.note.content} action={editNote} />;
+};
+
+export default EditNote;
